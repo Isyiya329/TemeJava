e6fb74b2d86dc7ea49000f3a8948e9dc9a622f7c
Stolojanu Maria-Denisa <denisa17stolojanu@gmail.com>
2025-01-17 13:03:58 +0200

    Add files via upload

---
+++ Laborator5/src/main/java/Problema3/Mobilier.java
@@ -0,0 +1,41 @@
+package Problema3;
+
+import java.util.List;
+
+public class Mobilier
+{
+    private String nume;
+    private List<Placa> placi;
+
+    public Mobilier(String nume, List<Placa> placi)
+    {
+        this.nume = nume;
+        this.placi = placi;
+    }
+
+    public Mobilier() {
+
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public List<Placa> getPlaci() {
+        return placi;
+    }
+
+    public void setPlaci(List<Placa> placi) {
+        this.placi = placi;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Nume: " + nume + " Placi: " + placi;
+    }
+}
